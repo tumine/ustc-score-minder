76cc2be8e6b3d4a5073177e38fa7910d8b89cff6
tumine <13306902255@189.cn>
2026-02-20 23:02:50 +0800

    更改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ustc.scoreminder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1.2"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
